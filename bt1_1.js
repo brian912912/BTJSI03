47a0282168229d03ddb062335c9e8dc09736ad0c
brian912912 <hovanngoc912@gmail.com>
2023-12-05 20:53:51 +0700

    bt bủi 1 nee

---
+++ bt1_1.js
@@ -0,0 +1,28 @@
+arr = [1, 3, 2, 5, 7, 4, 5, 9, 12, 18, 17];
+
+// câu a tổng của mảng 
+let sum = arr.reduce((a, b) => a + b, 0);
+console.log("Tổng của mảng là: " + sum);
+
+// câu b tìm phần tử lớn nhất bé nhất của mảng
+let max = Math.max(...arr);
+let min = Math.min(...arr);
+console.log("Phần tử lớn nhất là: " + max);
+console.log("Phần tử nhỏ nhất là: " + min);
+
+
+// c đảo ngược mảng 
+arr.reverse();
+console.log("Mảng sau khi đảo ngược là: " + arr);
+
+
+// d loại bỏ phần tử trùng nhau trong mảng
+arr.reverse();
+console.log("Mảng sau khi đảo ngược là: " + arr);
+
+
+// e tìm số chẵn và lẽ 
+let sole = arr.filter(num => num % 2 !== 0);
+let sochan = arr.filter(num => num % 2 === 0);
+console.log("Các số lẻ trong mảng là: " + sole);
+console.log("Các số chẵn trong mảng là: " + sochan);
---
+++ bt1_2.js
@@ -0,0 +1,48 @@
+//a tạo obj :)))
+let nguoi = {
+    name: "Ngocdtrai",
+    age: 14,
+    address: "nhà ở mindX"
+};
+
+
+// b truy suất thuộc tính
+function getInfo(obj) {
+    return "Tên: " + obj.name + ", Tuổi: " + obj.age;
+}
+console.log(getInfo(nguoi));
+
+//c thêm và xóa thuộc tính
+function addProperty(obj, propName, propValue) {
+    obj[propName] = propValue;
+}
+
+function deleteProperty(obj, propName) {
+    delete obj[propName];
+}
+
+addProperty(nguoi, "job", "Developer");
+console.log(nguoi);
+
+deleteProperty(nguoi, "address");
+console.log(nguoi);
+
+
+// đối tượng lồng nhau
+let nuoc = {
+    name: "Vietnam",
+    danso: 100000000,
+    thudo: {
+        name: "Hanoi",
+        danso: 8000000
+    }
+};
+
+//Liệt kê thuộc tính
+function listProperties(obj) {
+    for (let prop in obj) {
+        console.log(prop + ": " + obj[prop]);
+    }
+}
+
+listProperties(country);
