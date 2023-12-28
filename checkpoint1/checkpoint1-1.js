3d0bf3f6f10f01adb55cbb8e2100545952edd56b
brian912912 <hovanngoc912@gmail.com>
2023-12-28 20:13:50 +0700

    checkpoint1 nay 10đ k cần chấm

---
+++ checkpoint1/checkpoint1-1.js
@@ -0,0 +1,43 @@
+// CÂU 9 xóa ẩn tử k phải number r bình phương lên
+let m = [1, 2, 3, 4, 5, 6, "hh", "9", 80, 100];
+let mbinhphuong = m.filter(item => typeof item === 'number').map(item => Math.pow(item, 2));
+console.log(mbinhphuong);
+
+
+
+// CÂU 10 chuyển string thành array rồi xóa bỏ các phần tử có giá trị là "" và các chữ hoa chuyển thành chữ thường
+let str = "High knowledge, high return";
+let arr = str.split(" ").filter(item => item !== "").map(item => item.toLowerCase());
+console.log(arr);
+
+
+
+// CÂU 11 hệ thống quản lý nhân viên như sau 
+class Employee {
+    constructor(name, salary) {
+      (this.name = name),(this.salary = salary)
+    }
+  
+    displayInfo() {
+      console.log(`Employee: ${this.name}, Salary: ${this.salary}`);
+    }
+  }
+  
+  class Manager extends Employee {
+    constructor(name, salary, department) {
+      super(name, salary),(this.department = department)
+    }
+  
+    displayInfo() {
+      super.displayInfo();
+      console.log(`Manager of ${this.department} department`);
+    }
+  }
+  
+  // thực hiện thử nee :)
+  let emp1 = new Employee("Huy beu", -5000);
+  let manager1 = new Manager("VNgoc deptrai", 8000, "MRK");
+  
+  emp1.displayInfo(); 
+  manager1.displayInfo(); 
+  
